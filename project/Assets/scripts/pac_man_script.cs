048a0180d119851be8a7a89b6b89e5170f406c48
Hunter Bradley <48698364+3deltastorm@users.noreply.github.com>
2020-03-02 17:23:03 -0800

    PacMan can move!
    PacMan can move, kind of. Needs to be fixed to work with the trigger tiles.

--- project/Assets/scripts/pac_man_script.cs
+++ project/Assets/scripts/pac_man_script.cs
@@ -26,62 +26,62 @@ public class pac_man_script : MonoBehaviour
 
     Vector2 movement;
 
+    public Rigidbody2D rigidbody2d;
+
     // Start is called before the first frame update
     void Start()
     {
         lastInput = 2;
 
-        movement = RigidBody2D.
+        rigidbody2d = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        movement = rigidbody2d.velocity;
+
         //If the Up key is pressed, Set the lastInput to Up
-        if (Input.GetKeyDown(KeyCode.UpArrow) && lastInput != 0)
+        if (Input.GetKeyDown(KeyCode.UpArrow) /*&& lastInput != 0*/)
         {
             lastInput = 0;
 
-            movement.y = 1.0f * pacSpeed;
-            movement.x = 0.0f;
+            movement.y = movement.y + 1.0f * pacSpeed * Time.deltaTime;
 
             Debug.Log(lastInput);
         }
 
         //If the Down key is pressed, Set the lastInput to Down
-        else if (Input.GetKeyDown(KeyCode.DownArrow) && lastInput != 1)
+        else if (Input.GetKeyDown(KeyCode.DownArrow) /*&& lastInput != 1*/)
         {
             lastInput = 1;
 
-            movement.y = -1.0f * pacSpeed;
-            movement.x = 0.0f;
+            movement.y = movement.y - 1.0f * pacSpeed * Time.deltaTime;
 
             Debug.Log(lastInput);
         }
 
         //If the Right key is pressed, Set the lastInput to Right
-        else if (Input.GetKeyDown(KeyCode.RightArrow) && lastInput != 2)
+        else if (Input.GetKeyDown(KeyCode.RightArrow) /*&& lastInput != 2*/)
         {
             lastInput = 2;
 
-            movement.x = 1.0f * pacSpeed;
-            movement.y = 0.0f;
+            movement.x = movement.x + 1.0f * pacSpeed * Time.deltaTime;
 
             Debug.Log(lastInput);
         }
 
         //If the Left key is pressed, Set the lastInput to Left
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) && lastInput != 3)
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) /*&& lastInput != 3*/)
         {
             lastInput = 3;
 
-            movement.x = -1.0f * pacSpeed;
-            movement.y = 0.0f;
+            movement.x = movement.x - 1.0f * pacSpeed * Time.deltaTime;
 
             Debug.Log(lastInput);
         }
 
-        
+        rigidbody2d.velocity = movement;
 
     }
 
